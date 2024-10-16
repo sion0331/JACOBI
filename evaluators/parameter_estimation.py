c9f2b9f95ff6a859ffd91f30b1e9952caad8a6d4
Sion Chun <sion0331@gmail.com>
2024-10-16 17:12:53 -0400

    txt file generation fix for betas

--- evaluators/parameter_estimation.py
+++ evaluators/parameter_estimation.py
@@ -1,10 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint
 from scipy.optimize import minimize
-import matplotlib.pyplot as plt
 import re
-
-from utils import models
 
 
 def load_system(filename):
